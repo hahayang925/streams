2f245d03c4d67ed4e70ca6e1f87ace451379250c
Kim <kim.yang@waveo.com>
2019-08-19 23:04:25 +0800

    setup google oauth2

---
+++ src/actions/index.js
@@ -0,0 +1,23 @@
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
+import stream from '../apis/stream';
+
+export const signIn = (userId) => {
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT,
+  }
+};
+
+export const createStream = (formValues) => {
+  return async (dispatch) => {
+    const response = await stream.post('/streams', formValues);
+
+    dispatch({type: CREATE_STREAM, payload: response.data});
+  };
+}
