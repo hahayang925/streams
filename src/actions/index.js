dc7680e974f4f45cd55b59547a43bd0a77225442
Kim <kim.yang@waveo.com>
2019-08-24 23:44:26 +0800

    Update StreamEdit & StreamList

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,14 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from './types';
-import stream from '../apis/stream';
+import streams from '../apis/stream';
+import history from '../history';
+import { 
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+} from './types';
 
 export const signIn = (userId) => {
   return {
@@ -15,9 +24,44 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-  return async (dispatch) => {
-    const response = await stream.post('/streams', formValues);
+  return async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const response = await streams.post('/streams', { ...formValues, userId });
 
     dispatch({type: CREATE_STREAM, payload: response.data});
+    // DO some programatic navigation to get the user back to the root route
+    history.push('/');
   };
+}
+
+export const fetchStreams = () => {
+  return async (dispatch) => {
+    const response = await streams.get('/streams');
+
+    dispatch({type: FETCH_STREAMS, payload: response.data});
+  };
+}
+
+export const fetchStream = (id) => {
+  return async (dispatch) => {
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({type: FETCH_STREAM, payload: response.data});
+  };
+}
+
+export const editStream = (id, formValues) => {
+  return async (dispatch) => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+
+    dispatch({type: EDIT_STREAM, payload: response});
+  };
+}
+
+export const deleteStream = (id) => {
+  return async (dispatch) => {
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({type: DELETE_STREAM, payload: id});
+  }
 }
